a00a2c900d7ba3758a9773ae947641848f41f8eb
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2025-04-17 20:53:28 +0200

    feat: Add LazyColumn for categories in HomeScreen
    This commit replaces the Row layout with a LazyColumn in the HomeScreen composable to display categories. This change improves performance by only composing visible items and adds vertical scrolling for a potentially large number of categories. Additionally, padding is added around the LazyColumn.

--- app/src/main/java/com/mundocode/moneyflow/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/mundocode/moneyflow/ui/screens/home/HomeScreen.kt
@@ -3,14 +3,17 @@ package com.mundocode.moneyflow.ui.screens.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -150,11 +153,13 @@ fun FiltroPorCategoria(
     val categorias by viewModel.categoriasDisponibles.collectAsState(initial = listOf(stringResource(R.string.all)))
     var categoriaSeleccionada by remember { mutableStateOf(context.getString(R.string.all)) }
 
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(16.dp)
     ) {
-        categorias.forEach { categoria ->
+
+        items(categorias) { categoria ->
             Button(
                 onClick = {
                     categoriaSeleccionada = categoria
